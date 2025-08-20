db67cfbd6da6ed0359b4a9ea11d1a82cc273d998
ElvisTheDuke <ologehelvis@gmail.com>
2025-08-20 14:13:17 +0100

    about section

--- src/components/AboutSection.tsx
+++ src/components/AboutSection.tsx
@@ -1,7 +1,7 @@
 import { TextParallaxContent } from "../utils/ParallaxSection";
 import about1 from "../assets/about1.png";
-// import about2 from "../assets/about2.png";
-// import about3 from "../assets/about3.png";
+import about2 from "../assets/about2.png";
+import about3 from "../assets/about3.png";
 
 function AboutSection() {
   return (
@@ -30,6 +30,44 @@ function AboutSection() {
               discussions, and transform concepts into impactful solutions,
               empowering teams to innovate and achieve goals with collective
               brilliance.
+            </p>
+          </div>
+        </div>
+      </TextParallaxContent>
+      <TextParallaxContent
+        imgUrl={about2}
+        subheading="Starlink High-Speed Internet"
+        heading="Lightning-fast, reliable connectivity that never slows down your workflow"
+      >
+        <div className="container py-12 mx-auto flex text-white">
+          <div className="flex-2 flex flex-col items-end p-8 text-[#00CC4B] font-bold text-5xl">
+            <p className="max-w-[400px] text-end">BLAZING-FAST CONNECTIVITY</p>
+          </div>
+          <div className="flex-3 p-8 text-2xl">
+            <p className="max-w-[600px]">
+              Power through tasks with ultra-speed connections that eliminate
+              delays, accelerate performance, and keep workflows running
+              smoothly, ensuring maximum efficiency and uninterrupted progress
+              from start to finish.
+            </p>
+          </div>
+        </div>
+      </TextParallaxContent>
+      <TextParallaxContent
+        imgUrl={about3}
+        subheading="24/7 Power Supply"
+        heading="Uninterrupted productivity with enterprise-grade power infrastructure"
+      >
+        <div className="container py-12 mx-auto flex text-white">
+          <div className="flex-2 flex flex-col items-end p-8 text-[#00CC4B] font-bold text-5xl">
+            <p className="max-w-[400px] text-end">UNINTERRUPTED PRODUCTIVITY</p>
+          </div>
+          <div className="flex-3 p-8 text-2xl">
+            <p className="max-w-[600px]">
+              With enterprise-grade power infrastructure, your workstation stays
+              active around the clock, ensuring seamless operations, zero
+              downtime, and the confidence to focus fully on achieving your
+              goals.
             </p>
           </div>
         </div>
